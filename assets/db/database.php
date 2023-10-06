773b315f3c021933b542fb271d68e03a7ccc8331
DamianoCasolari <damianocasolari@gmail.com>
2023-10-06 10:14:22 +0200

    create scafolding

---
+++ assets/db/database.php
@@ -0,0 +1,15 @@
+ <?php
+    $db_host = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "root";
+    $db_name = "db_edusogno";
+
+    $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+    if ($conn->connect_error) {
+        die("Connessione fallita: " . $conn->connect_error);
+    }
+
+    echo "Connessione riuscita";
+
+    $conn->close();
